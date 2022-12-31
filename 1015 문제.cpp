518bab55137fc0f2c910990c3872232ff2c070ba
su bin choi <subil0306ab@gmail.com>
2022-12-31 20:55:12 +0900

    문제

---
+++ 1015 문제.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+ 	int a;
+    scanf("%d%d%d",&a);
+    printf("%d %d %d",a);
+ 	
+	return 0;	
+} 
---
+++ 1019 문제.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+ 	int a;
+ 	scnaf("%d.%d",&a);
+ 	printf("%d\n%d",a);
+ 	
+	return 0;	
+} 
+
+
